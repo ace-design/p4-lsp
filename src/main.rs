35c6ecb49d19d70383a80fe22afedbd20e016cae
Lachance, Alexandre <alexandrelachance@me.com>
2023-02-13 17:03:57 -0500

    Read files in workspace.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
+use std::fs;
+use std::path::PathBuf;
+
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
-#[derive(Debug)]
+use tree_sitter::{Parser, Tree};
+
+struct File {
+    path: PathBuf,
+    content: String,
+    tree: Option<Tree>,
+}
+
 struct Backend {
     client: Client,
+    parser: Parser,
+    files: Option<Vec<File>>,
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+    async fn initialize(&self, params: InitializeParams) -> Result<InitializeResult> {
+        let path = params.root_uri.unwrap().to_file_path().unwrap();
+
+        let paths = fs::read_dir(path).unwrap();
+
+        let p4_files = paths
+            .into_iter()
+            .filter(|file| file.as_ref().unwrap().path().extension().unwrap() == "p4")
+            .map(|file| {
+                let file_path = file.unwrap().path();
+                let file_content = fs::read_to_string(file_path.clone()).unwrap();
+
+                File {
+                    path: file_path,
+                    content: file_content,
+                    tree: None,
+                }
+            })
+            .collect::<Vec<File>>();
+
+        self.client
+            .log_message(MessageType::INFO, p4_files.len())
+            .await;
+
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
                 hover_provider: Some(HoverProviderCapability::Simple(true)),
                 completion_provider: Some(CompletionOptions::default()),
-                workspace: Some(WorkspaceServerCapabilities {
-                    workspace_folders: Some(WorkspaceFoldersServerCapabilities {
-                        supported: Some(true),
-                        change_notifications: Some(OneOf::Left(true)),
-                    }),
-                    file_operations: None,
-                }),
                 ..Default::default()
             },
             ..Default::default()
@@ -38,24 +66,6 @@ impl LanguageServer for Backend {
             .log_message(MessageType::INFO, "server stopped!")
             .await;
         Ok(())
-    }
-
-    async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
-        self.client
-            .log_message(MessageType::INFO, "workspace folders changed!")
-            .await;
-    }
-
-    async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
-        self.client
-            .log_message(MessageType::INFO, "configuration changed!")
-            .await;
-    }
-
-    async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
-        self.client
-            .log_message(MessageType::INFO, "watched files have changed!")
-            .await;
     }
 
     async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
@@ -85,6 +95,13 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let mut parser = Parser::new();
+    parser.set_language(tree_sitter_p4::language()).unwrap();
+
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        parser,
+        files: None,
+    });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
