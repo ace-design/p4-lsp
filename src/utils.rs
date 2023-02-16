01f35e85980ba168a67e663aa4f69025f8317658
Lachance, Alexandre <alexandrelachance@me.com>
2023-02-16 13:05:04 -0500

    Implement pos_to_byte.

--- src/utils.rs
+++ src/utils.rs
@@ -9,5 +9,44 @@ pub fn pos_to_point(pos: Position) -> Point {
 }
 
 pub fn pos_to_byte(pos: Position, text: &str) -> usize {
-    todo!()
+    let mut total_bytes = 0;
+    let lines = &text.lines().collect::<Vec<&str>>()[..(pos.line as usize)];
+
+    for line in lines {
+        total_bytes += line.len() + 1;
+    }
+    total_bytes + pos.character as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use tower_lsp::lsp_types::Position;
+
+    use super::pos_to_byte;
+
+    #[test]
+    fn test_pos_to_byte() {
+        let string = "this\nis\na test\nfor this function";
+
+        assert_eq!(
+            pos_to_byte(
+                Position {
+                    character: 3,
+                    line: 2
+                },
+                string
+            ),
+            11
+        );
+        assert_eq!(
+            pos_to_byte(
+                Position {
+                    character: 5,
+                    line: 0
+                },
+                string
+            ),
+            5
+        );
+    }
 }
