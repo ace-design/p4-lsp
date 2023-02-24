0b0923f96bff4daf2821c9af050993614ff20e78
Lachance, Alexandre <alexandrelachance@me.com>
2023-02-24 17:12:07 -0500

    Moving parsing of scope to its own function.

--- src/scope_tree.rs
+++ src/scope_tree.rs
@@ -2,10 +2,14 @@ use std::sync::{Arc, Mutex};
 
 use tree_sitter::{Node, Tree};
 
+pub struct Scope {
+    variables: Vec<String>,
+}
+
 pub struct ScopeNode {
     pub scope: Scope,
     pub children: Vec<Arc<Mutex<ScopeNode>>>,
-    pub parent: Option<Arc<Mutex<ScopeNode>>>,
+    pub parent: Arc<Mutex<Option<ScopeNode>>>,
 }
 
 impl ScopeNode {
@@ -14,39 +18,35 @@ impl ScopeNode {
             return None;
         }
         let tree = tree.unwrap();
-        let cursor = &mut tree.root_node().walk();
 
-        let mut variables: Vec<String> = vec![];
-
-        for child in tree.root_node().named_children(cursor) {
-            match child.kind() {
-                "constant_declaration" => {
-                    let name_node = child.child_by_field_name("name").unwrap();
-                    let name_range = name_node.range();
-
-                    let name: String =
-                        content[name_range.start_byte..name_range.end_byte].to_string();
-
-                    variables.push(name);
-                }
-                _ => {}
-            }
-        }
-
-        Some(ScopeNode {
-            scope: Scope { variables },
-            children: vec![],
-            parent: None,
-        })
+        Some(parse_scope(None, tree.root_node(), content))
     }
-
-    fn parse_scope(node: Node, content: &str) {}
 
     pub fn variables_in_scope(&self) -> Vec<String> {
         self.scope.variables.clone()
     }
 }
 
-pub struct Scope {
-    variables: Vec<String>,
+fn parse_scope(parent_node: Option<ScopeNode>, current_node: Node, content: &str) -> ScopeNode {
+    let cursor = &mut current_node.walk();
+    let mut variables: Vec<String> = vec![];
+    for child in current_node.named_children(cursor) {
+        match child.kind() {
+            "constant_declaration" => {
+                let name_node = child.child_by_field_name("name").unwrap();
+                let name_range = name_node.range();
+
+                let name: String = content[name_range.start_byte..name_range.end_byte].to_string();
+
+                variables.push(name);
+            }
+            _ => {}
+        }
+    }
+
+    ScopeNode {
+        scope: Scope { variables },
+        children: vec![],
+        parent: Arc::from(Mutex::from(parent_node)),
+    }
 }
