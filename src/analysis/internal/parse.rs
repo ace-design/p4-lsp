94eba4d2fadc6cab46afdbc9856e519d6723509c
Lachance, Alexandre <alexandrelachance@me.com>
2023-03-22 13:53:46 -0400

    Add more detail to parse analysis diagnostics.

--- src/analysis/internal/parse.rs
+++ src/analysis/internal/parse.rs
@@ -1,7 +1,7 @@
 use crate::analysis::Analysis;
 use crate::file::File;
 
-use tower_lsp::lsp_types::Diagnostic;
+use tower_lsp::lsp_types::{Diagnostic, DiagnosticSeverity};
 
 pub struct Parse {}
 
@@ -13,7 +13,17 @@ impl Analysis for Parse {
             .into_iter()
             .map(|node| {
                 let err_msg = node.get_error_msg().unwrap_or("Parsing error.".into());
-                Diagnostic::new_simple(node.range, err_msg)
+                Diagnostic::new(
+                    node.range,
+                    Some(DiagnosticSeverity::ERROR),
+                    Some(tower_lsp::lsp_types::NumberOrString::String(
+                        "parsing".to_string(),
+                    )),
+                    Some("AST".to_string()),
+                    err_msg.trim().to_string(),
+                    None,
+                    None,
+                )
             })
             .collect()
     }
