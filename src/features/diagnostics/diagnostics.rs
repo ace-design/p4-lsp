8366255feaefcbe651e459bf152ee9496a877c5d
Lachance, Alexandre <alexandrelachance@me.com>
2023-04-05 16:14:20 -0400

    Add diags macro and structure for symbols diagnostic.

--- src/features/diagnostics/diagnostics.rs
+++ src/features/diagnostics/diagnostics.rs
@@ -2,22 +2,34 @@ use crate::File;
 use tower_lsp::lsp_types::Diagnostic;
 
 use super::external::P4Test;
-use super::internal::Parse;
+use super::internal::{Parse, Symbols};
+
+macro_rules! diags {
+    ($($diag:expr),*) => {
+        {
+            let mut diags = Vec::new();
+
+            $(
+                diags.append(&mut $diag);
+            )*
+
+            diags
+        }
+    };
+}
 
 pub trait DiagnosticProvider {
     fn get_diagnostics(file: &File) -> Vec<Diagnostic>;
 }
 
 pub fn get_quick_diagnostics(file: &File) -> Vec<Diagnostic> {
-    let diagnotics = Parse::get_diagnostics(file);
-
-    diagnotics
+    diags![Parse::get_diagnostics(file), Symbols::get_diagnostics(file)]
 }
 
 pub fn get_full_diagnostics(file: &File) -> Vec<Diagnostic> {
-    let mut diagnotics = P4Test::get_diagnostics(file);
-
-    diagnotics.append(&mut Parse::get_diagnostics(file));
-
-    diagnotics
+    diags![
+        P4Test::get_diagnostics(file),
+        Parse::get_diagnostics(file),
+        Symbols::get_diagnostics(file)
+    ]
 }
