629c1897b41852703502af95714aab521362b600
MaelC001 <55336346+MaelC001@users.noreply.github.com>
2023-06-20 09:51:32 -0400

    update

--- src/metadata/metadata.rs
+++ src/metadata/metadata.rs
@@ -9,9 +9,10 @@ pub struct Metadata {
 impl Metadata {
     pub fn new(source_code: &str, syntax_tree: tree_sitter::Tree) -> Option<Metadata> {
         let ast = Ast::new(source_code, syntax_tree)?;
+        debug!("\nAST:\n{}", ast);
         let symbol_table = SymbolTable::new(&ast);
-        debug!("\nAST:\n{}\nSymbol Table:\n{}", ast, symbol_table);
+        debug!("\nSymbol Table:\n{}", symbol_table);
 
-        Some(Metadata { ast, symbol_table })
+        Some(Metadata { ast : ast, symbol_table : symbol_table })
     }
 }
