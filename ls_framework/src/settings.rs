e5b45e286cbb028c0a0e7c2b9aeae1176d44e5cb
Lachance, Alexandre <alexandrelachance@me.com>
2023-08-10 17:01:52 -0400

    Remove P4 specific settings from framework.

--- ls_framework/src/settings.rs
+++ ls_framework/src/settings.rs
@@ -1,35 +1,15 @@
 use serde_json::Value;
-use std::path::PathBuf;
 
 #[derive(Debug, Default)]
-pub struct Settings {
-    pub include_path: Option<PathBuf>,
-    pub p4test_path: Option<PathBuf>,
-}
+pub struct Settings {}
 
 impl Settings {
     pub fn parse(value: Value) -> Settings {
-        if let Value::Object(map) = value {
-            let include_path: Option<PathBuf> =
-                if let Some(Value::String(path_str)) = map.get("include_path") {
-                    Some(PathBuf::from(path_str))
-                } else {
-                    None
-                };
-            let p4test_path: Option<PathBuf> =
-                if let Some(Value::String(path_str)) = map.get("p4test_path") {
-                    Some(PathBuf::from(path_str))
-                } else {
-                    None
-                };
-
-            Settings {
-                include_path,
-                p4test_path,
-            }
+        if let Value::Object(_map) = value {
+            Settings {}
         } else {
             Settings {
-                ..Default::default()
+                // ..Default::default()
             }
         }
     }
