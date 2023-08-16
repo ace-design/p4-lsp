5faa7ae398f81e63f84571ec464b39d514ed7ec1
Lachance, Alexandre <alexandrelachance@me.com>
2023-08-16 14:16:19 -0400

    Remove function from other function.

--- ls_framework/src/features/completion.rs
+++ ls_framework/src/features/completion.rs
@@ -6,37 +6,37 @@ use crate::{
 };
 use tower_lsp::lsp_types::{CompletionItem, CompletionItemKind, Position};
 
+fn default_list(
+    position: Position,
+    query: &Arc<Mutex<impl SymbolTableQuery>>,
+) -> Option<Vec<CompletionItem>> {
+    let symbols: Symbols = query.lock().unwrap().get_symbols_at_pos(position);
+
+    let mut items: Vec<CompletionItem> = Vec::new();
+
+    for symbol_def in &LanguageDefinition::get().symbol_types {
+        if let Some(list) = symbols.get_type(symbol_def.name.clone()) {
+            items.append(
+                &mut list
+                    .iter()
+                    .map(|item| CompletionItem {
+                        label: item.get_name(),
+                        kind: Some(symbol_def.completion_type.get()),
+                        ..Default::default()
+                    })
+                    .collect(),
+            );
+        }
+    }
+
+    Some(items)
+}
+
 pub fn get_list(
     position: Position,
     source_code: &str,
     query: &Arc<Mutex<impl SymbolTableQuery>>,
 ) -> Option<Vec<CompletionItem>> {
-    fn default(
-        position: Position,
-        query: &Arc<Mutex<impl SymbolTableQuery>>,
-    ) -> Option<Vec<CompletionItem>> {
-        let symbols: Symbols = query.lock().unwrap().get_symbols_at_pos(position);
-
-        let mut items: Vec<CompletionItem> = Vec::new();
-
-        for symbol_def in &LanguageDefinition::get().symbol_types {
-            if let Some(list) = symbols.get_type(symbol_def.name.clone()) {
-                items.append(
-                    &mut list
-                        .iter()
-                        .map(|item| CompletionItem {
-                            label: item.get_name(),
-                            kind: Some(symbol_def.completion_type.get()),
-                            ..Default::default()
-                        })
-                        .collect(),
-                );
-            }
-        }
-
-        Some(items)
-    }
-
     let name_field = query.lock().unwrap().get_name_field(position, source_code);
 
     match name_field {
@@ -52,6 +52,6 @@ pub fn get_list(
                     .collect(),
             )
         }
-        None => default(position, query),
+        None => default_list(position, query),
     }
 }
