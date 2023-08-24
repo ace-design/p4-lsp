657b331410460a412a6170bfdbcf8e82a162dd48
MaelC001 <55336346+MaelC001@users.noreply.github.com>
2023-08-24 15:40:37 -0400

    change how the command is done + get all the result not only the first one

--- plugin/p4test/src/main.rs
+++ plugin/p4test/src/main.rs
@@ -1,92 +1,108 @@
-use extism_pdk::*;
 use lsp_types::{Diagnostic, DiagnosticSeverity, Position, Range};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::io::*;
+use std::process::{Command, Stdio};
 
-extern "C" {
-    fn host_command(ptr: i64) -> i64;
+fn get_command_output(command: &str, current_dir: &str, args: Vec<String>) -> (Vec<u8>, Vec<u8>) {
+    let mut partial_command = Command::new(command);
+
+    if !args.is_empty() {
+        partial_command.args(args);
+    }
+
+    if current_dir != "" {
+        partial_command.current_dir(current_dir);
+    }
+
+    let command_result = partial_command
+        .stderr(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn();
+
+    if let Ok(command) = command_result {
+        let result = command.wait_with_output().unwrap();
+
+        (result.stdout, result.stderr)
+    } else {
+        (vec![], vec![])
+    }
 }
 
-#[derive(Serialize, Deserialize)]
-struct CommandOutput {
-    stdout: Vec<u8>,
-    stderr: Vec<u8>,
-}
+// [{"key":"workspace","value":"/home/t/p4-lsp"},{"key":"file","value":"/home/mc/Documents/p4-lsp/examples/casts.p4"}]
 
 pub fn diagnostic(file_path: String) -> String {
-    let command = format!("p4test {}", file_path);
-    let memory = Memory::from_bytes(&command);
-    let offset = unsafe { host_command(memory.offset as i64) };
+    let (stdout, stderr) = get_command_output("p4test", "", vec![file_path.clone()]);
 
-    let out: CommandOutput =
-        serde_json::from_str(get_string(offset as u64).unwrap().as_str()).unwrap();
-
-    serde_json::to_string(&parse_output(
-        String::from_bytes(out.stderr).unwrap(),
-        file_path,
-    ))
-    .unwrap()
+    let mut t: Vec<Diagnostic> = Vec::new();
+    if !stderr.is_empty() {
+        t = parse_output(String::from_utf8(stderr).unwrap(), file_path)
+    }
+    serde_json::to_string(&t).unwrap()
 }
 
 fn parse_output(message: String, file_path: String) -> Vec<Diagnostic> {
     // Parse and remove line number
-    let line_nb_re = Regex::new(format!(r"{}\((\d+)\):?", file_path).as_str()).unwrap();
-    let captures = line_nb_re.captures(&message).unwrap();
-    let line_nb = captures
-        .get(1)
-        .unwrap()
-        .as_str()
-        .parse::<u32>()
-        .ok()
-        .unwrap()
-        - 1;
-    let current_msg = line_nb_re.replace(&message, "");
-
-    let kind_re = Regex::new(r"\[--W(.*)=(.*)\]").unwrap();
-    let captures = kind_re.captures(&current_msg);
-
-    // Parse and remove severity and kind
-    let (severity, kind) = if let Some(captures) = captures {
-        let severity_capture = captures.get(1);
-        let severity = if let Some(cap) = severity_capture {
-            match cap.as_str() {
-                "error" => DiagnosticSeverity::ERROR,
-                "warn" => DiagnosticSeverity::WARNING,
-                _ => DiagnosticSeverity::ERROR,
-            }
-        } else {
-            DiagnosticSeverity::ERROR
-        };
-
-        let kind_cap = captures.get(2);
-        let kind = if let Some(cap) = kind_cap {
-            cap.as_str()
-        } else {
-            ""
-        };
-
-        (severity, kind)
-    } else {
-        (DiagnosticSeverity::ERROR, "")
-    };
-    let current_msg = kind_re.replace(&current_msg, "");
-
     // Make and return diagnostic
-    let lines: Vec<&str> = current_msg.trim().lines().collect();
+    let mut vec_diagnostic: Vec<Diagnostic> = vec![];
+    let lines: Vec<&str> = message.trim().lines().collect();
+    for index in (0..(lines.len())).step_by(3) {
+        let line = lines.get(index).unwrap();
+        let line_nb_re = Regex::new(format!(r"{}\((\d+)\):?", file_path.clone()).as_str()).unwrap();
+        let captures = line_nb_re.captures(&line).unwrap();
+        let line_nb = captures
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse::<u32>()
+            .ok()
+            .unwrap()
+            - 1;
+        let current_msg = line_nb_re.replace(&line, "");
 
-    let diag_msg = lines[0].replace("error:", "").replace("warning:", "");
-    let diag_range = get_range(line_nb, lines[2]);
+        let kind_re = Regex::new(r"\[--W(.*)=(.*)\]").unwrap();
+        let captures = kind_re.captures(&current_msg);
 
-    vec![Diagnostic::new(
-        diag_range,
-        Some(severity),
-        Some(lsp_types::NumberOrString::String(kind.to_string())),
-        Some("p4test".to_string()),
-        diag_msg.trim().to_string(),
-        None,
-        None,
-    )]
+        // Parse and remove severity and kind
+        let (severity, kind) = if let Some(captures) = captures {
+            let severity_capture = captures.get(1);
+            let severity = if let Some(cap) = severity_capture {
+                match cap.as_str() {
+                    "error" => DiagnosticSeverity::ERROR,
+                    "warn" => DiagnosticSeverity::WARNING,
+                    _ => DiagnosticSeverity::ERROR,
+                }
+            } else {
+                DiagnosticSeverity::ERROR
+            };
+
+            let kind_cap = captures.get(2);
+            let kind = if let Some(cap) = kind_cap {
+                cap.as_str()
+            } else {
+                ""
+            };
+
+            (severity, kind)
+        } else {
+            (DiagnosticSeverity::ERROR, "")
+        };
+        let current_msg = kind_re.replace(&current_msg, "");
+
+        let diag_msg = current_msg.replace("error:", "").replace("warning:", "");
+        let diag_range = get_range(line_nb, lines.get(index + 2).unwrap());
+        vec_diagnostic.push(Diagnostic::new(
+            diag_range,
+            Some(severity),
+            Some(lsp_types::NumberOrString::String(kind.to_string())),
+            Some("p4test".to_string()),
+            diag_msg.trim().to_string(),
+            None,
+            None,
+        ));
+    }
+
+    vec_diagnostic
 }
 
 fn get_range(line_nb: u32, arrows: &str) -> Range {
@@ -104,17 +120,6 @@ fn get_range(line_nb: u32, arrows: &str) -> Range {
         Position::new(line_nb, start),
         Position::new(line_nb, arrows.len() as u32),
     )
-}
-
-fn get_string(offset: u64) -> FnResult<String> {
-    let length = unsafe { extism_pdk::bindings::extism_length(offset) };
-    let memory = Memory {
-        offset,
-        length,
-        free: false,
-    };
-
-    Ok(memory.to_string().unwrap())
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -144,7 +149,7 @@ pub fn main() {
 
     if p4 != "" {
         let json = diagnostic(p4);
-        println!("{{\"output\":\"diagnostic\", \"data\":\"{}\"}}", json);
+        println!("{{\"output_type\":\"Diagnostic\", \"data\":\"{}\"}}", json);
     } else {
         let json = serde_json::to_string(&Notification {
             message: "p4test testing : fail.\\nYou didn't give me all the arguments that I need."
@@ -152,6 +157,9 @@ pub fn main() {
             data: "".to_string(),
         })
         .unwrap();
-        println!("{{\"output\":\"notification\", \"data\":\"{}\"}}", json);
+        println!(
+            "{{\"output_type\":\"Notification\", \"data\":\"{}\"}}",
+            json
+        );
     }
 }
